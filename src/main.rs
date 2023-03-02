d7b8071414b4863ca314df24e639e0ed0d53260f
Neutronic John <jonbruskin@gmail.com>
2023-03-02 16:08:54 +0100

    Added Khronos validation layers to instance

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,17 @@ use anyhow::{anyhow, Result};
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 use vulkanalia::window as vk_window;
 use vulkanalia::prelude::v1_0::*;
+use vulkanalia::vk::ExtDebugUtilsExtension;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
+
+use std::collections::HashSet;
+use std::ffi::CStr;
+use std::os::raw::c_void;
+
+use log::*;
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -47,6 +54,28 @@ fn main() -> Result<()> {
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    const VALIDATION_ENABLED: bool =
+        cfg!(debug_assertions);
+
+    const VALIDATION_LAYER: vk::ExtensionName =
+        vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
+
+    let available_layers = entry
+        .enumerate_instance_layer_properties()?
+        .iter()
+        .map(|l| l.layer_name)
+        .collect::<HashSet<_>>();
+
+    if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) {
+        return Err(anyhow!("Validation layer requested but not supported."));
+    }
+
+    let layers = if VALIDATION_ENABLED {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        Vec::new()
+    };
+
     let application_info = vk::ApplicationInfo::builder()
         .application_name(b"Vulkan Tutorial\0")
         .application_version(vk::make_version(1, 0, 0))
@@ -55,13 +84,14 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
         .api_version(vk::make_version(1, 0, 0));
 
     let extensions = vk_window::get_required_instance_extensions(window)
-    .iter()
-    .map(|e| e.as_ptr())
-    .collect::<Vec<_>>();
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
 
     let info = vk::InstanceCreateInfo::builder()
-    .application_info(&application_info)
-    .enabled_extension_names(&extensions);
+        .application_info(&application_info)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions);
 
     return Ok(entry.create_instance(&info, None)?)
 }
