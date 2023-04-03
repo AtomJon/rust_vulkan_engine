e7613f3e8fb9c60559d17de605dbcfc26947cdb6
Neutronic John <jonbruskin@gmail.com>
2023-04-03 11:11:29 +0200

    Added a dummy operation to all command buffers

--- src/main.rs
+++ src/main.rs
@@ -223,6 +223,45 @@ unsafe fn create_command_buffers(device: &Device, data: &mut AppData) -> Result<
 
     data.command_buffers = device.allocate_command_buffers(&allocate_info)?;
 
+    for (i, command_buffer) in data.command_buffers.iter().enumerate() {
+        let inheritance = vk::CommandBufferInheritanceInfo::builder();
+    
+        let info = vk::CommandBufferBeginInfo::builder()
+            .flags(vk::CommandBufferUsageFlags::empty()) // Optional.
+            .inheritance_info(&inheritance);             // Optional.
+    
+        device.begin_command_buffer(*command_buffer, &info)?;
+
+        let render_area = vk::Rect2D::builder()
+            .offset(vk::Offset2D::default())
+            .extent(data.swapchain_extent);
+        
+        let color_clear_value = vk::ClearValue {
+            color: vk::ClearColorValue {
+                float32: [0.0, 0.0, 0.0, 1.0],
+            },
+        };
+            
+        let clear_values = &[color_clear_value];
+        let info = vk::RenderPassBeginInfo::builder()
+            .render_pass(data.render_pass)
+            .framebuffer(data.framebuffers[i])
+            .render_area(render_area)
+            .clear_values(clear_values);
+
+        device.cmd_begin_render_pass(
+            *command_buffer, &info, vk::SubpassContents::INLINE);    
+
+        device.cmd_bind_pipeline(
+            *command_buffer, vk::PipelineBindPoint::GRAPHICS, data.pipeline);
+
+        device.cmd_draw(*command_buffer, 3, 1, 0, 0);
+            
+        device.cmd_end_render_pass(*command_buffer);
+
+        device.end_command_buffer(*command_buffer)?;
+    }
+
     Ok(())
 }
 
