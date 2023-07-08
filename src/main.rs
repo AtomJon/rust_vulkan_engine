13ae5dd17acf9df38db965122e3429c4fd8a5497
Neutronic John <jonbruskin@gmail.com>
2023-07-08 17:21:15 +0200

    Added helper functions in vertex struct for creation binding and attribute descriptions

--- src/main.rs
+++ src/main.rs
@@ -97,6 +97,32 @@ impl Vertex {
     fn new(pos: glm::Vec2, color: glm::Vec3) -> Self {
         Self { pos, color }
     }
+
+    fn binding_description() -> vk::VertexInputBindingDescription {
+        vk::VertexInputBindingDescription::builder()
+            .binding(0)
+            .stride(size_of::<Vertex>() as u32)
+            .input_rate(vk::VertexInputRate::VERTEX)
+            .build()
+    }
+
+    fn attribute_descriptions() -> [vk::VertexInputAttributeDescription; 2] {
+        let pos = vk::VertexInputAttributeDescription::builder()
+            .binding(0)
+            .location(0)
+            .format(vk::Format::R32G32_SFLOAT)
+            .offset(0)
+            .build();
+
+        let color = vk::VertexInputAttributeDescription::builder()
+            .binding(0)
+            .location(1)
+            .format(vk::Format::R32G32B32_SFLOAT)
+            .offset(size_of::<glm::Vec2>() as u32)
+            .build();
+        
+        return [pos, color];
+    }
 }
 
 lazy_static! {
